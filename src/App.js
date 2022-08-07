5352ad27c8e7149f4932c3bd86276238d893d58c
Vikasmayya <65965704+Vikasmayya@users.noreply.github.com>
2022-08-07 13:35:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactGallery from "./ReactGallery";
+
+const App = () => {
+  return (
+    <>
+     <ReactGallery/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MenuItem.js
@@ -0,0 +1,79 @@
+const Menu = [
+    {
+       id: 1,
+       image: "images/1.png",
+       name:"maggi",
+       category: "breakfast",
+       price: "12₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+ 
+   {
+       id: 2,
+       image: "images/2.png",
+       name:"allu pakoda",
+       category: "evening",
+       price: "20₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+   {
+       id: 3,
+       image: "images/3.png",
+       name:"corn",
+       category: "breakfast",
+       price: "10₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+    {
+       id: 4,
+       image: "../images/4.png",
+       name:"chola",
+       category: "lunch",
+       price: "50₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+     {
+       id: 5,
+       image: "../images/5.png",
+       name:"pizza",
+       category: "evening",
+       price: "80₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+      {
+       id: 6,
+       image: "../images/6.png",
+       name:"Non-Veg Thali",
+       category: "dinner",
+       price: "180₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 7,
+       image: "../images/7.png",
+       name:"Sweets",
+       category: "dinner",
+       price: "60₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 8,
+       image: "../images/1.png",
+       name:"Rajma Rice",
+       category: "lunch",
+       price: "60₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 9,
+       image: "../images/2.png",
+       name:"samaso",
+       category: "evening",
+       price: "10₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+];
+
+
+
+export default Menu;
